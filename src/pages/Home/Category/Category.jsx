ea10c7623b70ee966c2d5c3600b46cfd03873d74
Amit Kumar Sutradhar <dev.amitkumarsutradhar@gmail.com>
2025-01-05 13:41:51 +0600

    Add home page category

---
+++ src/pages/Home/Category/Category.jsx
@@ -0,0 +1,68 @@
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
+import slide1 from '../../../assets/home/slide1.jpg';
+import slide2 from '../../../assets/home/slide2.jpg';
+import slide3 from '../../../assets/home/slide3.jpg';
+import slide4 from '../../../assets/home/slide4.jpg';
+import slide5 from '../../../assets/home/slide5.jpg';
+
+const Category = () => {
+    return (
+        <Swiper
+            slidesPerView={1}
+            spaceBetween={10}
+            pagination={{
+                clickable: true,
+            }}
+            breakpoints={{
+                640: {
+                    slidesPerView: 2,
+                    spaceBetween: 20,
+                },
+                768: {
+                    slidesPerView: 3,
+                    spaceBetween: 40,
+                },
+                1024: {
+                    slidesPerView: 4,
+                    spaceBetween: 50,
+                },
+            }}
+            modules={[Pagination]}
+            className="mySwiper pb-16"
+        >
+            <SwiperSlide>
+                <img src={slide1} alt="" />
+                <h3 className='text-3xl uppercase text-white -mt-16'>Saladas</h3>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={slide2} alt="" />
+                <h3 className='text-3xl uppercase text-white -mt-16'>Pizzas</h3>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={slide3} alt="" />
+                <h3 className='text-3xl uppercase text-white -mt-16'>Soups</h3>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={slide4} alt="" />
+                <h3 className='text-3xl uppercase text-white -mt-16'>Desserts</h3>
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={slide5} alt="" />
+                <h3 className='text-3xl uppercase text-white -mt-16'>Salads</h3>
+            </SwiperSlide>
+        </Swiper>
+    );
+};
+
+export default Category;
