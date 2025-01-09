b0d574c320aa3fecf9e95e2f77dca84c8f532b1b
Amit Kumar Sutradhar <dev.amitkumarsutradhar@gmail.com>
2025-01-09 17:36:35 +0600

    Page title setup with react-helmet-async

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     const navItems = <>
-        <li><a>Item 1</a></li>
+        <li><Link to={"/"}>Home</Link></li>
+        <li><Link to={"/menu"}>Our Menu</Link></li>
         <li>
             <a>Parent</a>
             <ul className="p-2">
